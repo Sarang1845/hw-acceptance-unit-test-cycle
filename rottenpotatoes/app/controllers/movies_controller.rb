135d2b1265187039a8eb2b44cbdc35226090bd21
EC2 Default User <ec2-user@ip-172-31-21-117.us-east-2.compute.internal>
2019-03-07 02:56:29 +0000

    Cucumber completed

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -59,6 +59,17 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def with_same_director
+    @movie = Movie.find(params[:id])
+    director_value = @movie.director
+    if director_value.present? then
+      @movies = Movie.where(:director => @movie.director)
+    else
+      flash[:notice] = "'#{@movie.title}' has no director info" 
+      redirect_to movies_path
+    end
   end
 
 end
